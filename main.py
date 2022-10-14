44e06a4ad9766ebe03f2fb3421dcb1e3ea87fe47
dartagnan sams <dartagnansams1@icloud.com>
2022-10-14 12:16:11 -0400

    switched to mongodb,added frontend js to create new hiders, next need to input pictures,and toggle if hider has been found

--- main.py
+++ main.py
@@ -8,79 +8,80 @@ from flask_socketio import (
 )  # replaces post requests
 from flaskext.mysql import MySQL #allows flask and mysql connection
 import os
-participants_names = ["Maya","Dart"]
+from pymongo import MongoClient
 
-participants_data = {
-    "Maya":{
-        "name":"Maya",
-        "status":"active"
-    },
-    "Dart":{
-        "name":"Dart",
-        "status":"active"
-    }
-}
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hello" 
 socketio = SocketIO(app)  # init the socket connection
-
-mysql = MySQL()  # to connect flask to mysql
-cleardb_str = "mysql://bde8974ff6848a:f82355c4@us-cdbr-east-06.cleardb.net/heroku_03c2a5e7c8a7f77?reconnect=true"
-app.config["MYSQL_DATABASE_HOST"] = "us-cdbr-east-06.cleardb.net"
-app.config["MYSQL_DATABASE_USER"] = "bde8974ff6848a"
-app.config["MYSQL_DATABASE_PASSWORD"] = "f82355c4"
-app.config["MYSQL_DATABASE_DB"] = "heroku_03c2a5e7c8a7f77"
-mysql.init_app(app)  # init the flask  to mysql connection
-
-conn=mysql.connect()
-mycursor=conn.cursor()
-
-def create_hider(hider_name):
-    mycursor.execute(f"CREATE TABLE {hider_name} (name VARCHAR(100),found VARCHAR(100),image_link VARCHAR(100))")
+db_str = "mongodb+srv://DartSams:Dartagnan_19@personal-cluser-db.qavgfkq.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(db_str)
+mydb = client["Personal_db"] #connects to db but if not found will create it 
+table1 = mydb["Hiders"] #connects to table but if not found will create it 
 
 
-# create_hider("Hiders")
+# mysql = MySQL()  # to connect flask to mysql
+# cleardb_str = "mysql://bde8974ff6848a:f82355c4@us-cdbr-east-06.cleardb.net/heroku_03c2a5e7c8a7f77?reconnect=true"
+# app.config["MYSQL_DATABASE_HOST"] = "us-cdbr-east-06.cleardb.net"
+# app.config["MYSQL_DATABASE_USER"] = "bde8974ff6848a"
+# app.config["MYSQL_DATABASE_PASSWORD"] = "f82355c4"
+# app.config["MYSQL_DATABASE_DB"] = "heroku_03c2a5e7c8a7f77"
+# mysql.init_app(app)  # init the flask  to mysql connection
+
+# conn=mysql.connect()
+# mycursor=conn.cursor()
+
+# def create_hider(hider_name):
+#     mycursor.execute(f"CREATE TABLE {hider_name} (name VARCHAR(100),found VARCHAR(100),image_link VARCHAR(100))")
 
 
-def insert_user(name,found="Not Yet",image_link="default link"):
-    # mycursor.execute("INSERT INTO Flask_Profile_Info (author,gender,age,job,location) VALUES (%s,%s%s,%s,%s)", ("iphone 69+","Dsams"))
-    # mycursor.execute("INSERT INTO Post_Table (author,post_date,post) VALUES (%s,%s,%s)", ("dartsams","Sat Jun 12 14:13:45 2021","i hate life"))
-    mycursor.execute("INSERT INTO Hiders (name,found,image_link) VALUES (%s,%s,%s)",(name,found,image_link))
-    conn.commit()
+# def insert_user(name,found="Not Yet",image_link="default link"):
+#     mycursor.execute("INSERT INTO Hiders (name,found,image_link) VALUES (%s,%s,%s)",(name,found,image_link))
+#     conn.commit()
 
-# insert_user("Maya","Not yet","Image")
 
-def show_entries(table_name):
-    user=[]
-    mycursor.execute(f"SELECT * FROM {table_name}")
-    for i in mycursor:
-        print(i)
-        user.append(i)
-        # return i
-    return user
+# def show_entries(table_name):
+#     user=[]
+#     mycursor.execute(f"SELECT * FROM {table_name}")
+#     for i in mycursor:
+#         print(i)
+#         user.append(i)
+#         # return i
+#     return user
 
-# show_entries('Hiders')
 
 @app.route("/")
 def index():
-
-    data = {
-        "participants_data":participants_data,
-        "participants_names":participants_names,
-        # "participants":[]
-    }
-    return render_template("index.html",data=data)
+    return render_template("index.html")
 
 @socketio.on("new-participant")
-def handle_message(data):
-    print(data)
-    insert_user(data["name"])
+def handle_message(msg):
+    print(msg)
+    data = {
+        "name":msg["name"],
+        "status":"Not yet",
+        "image_link":"Default link"
+        
+    }
+    table1.insert_one(data) #inserts into db
+    # client.close()
+    # insert_user(data["name"])
 
 @socketio.on("refresh")
 def handle_message(msg):
-    hider_lst = show_entries('Hiders')
+    print(msg)
+    # client = MongoClient(db_str)
+    # mydb = client["Personal_db"] #connects to db but if not found will create it 
+    # table1 = mydb["Hiders"] #connects to table but if not found will create it 
+    # print(table1)
+    hider_lst = []
+    for x in table1.find({},{"_id":0}):
+        print(x)
+        hider_lst.append(x)
     print(hider_lst)
+    # hider_lst = show_entries('Hiders')
+    # print(hider_lst)
     data = {
         "updated_list":hider_lst
     }
