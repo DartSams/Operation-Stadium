05569ffe231414b48bf73c5f623e105c6eefd449
dartagnan sams <dartagnansams1@icloud.com>
2022-10-13 21:31:32 -0400

    added sockets to get list of participants,added frontend functionality to enter new participants

--- db.py
+++ db.py
@@ -1,23 +1,47 @@
-import dotenv
+import mysql.connector
+from dotenv import load_dotenv
 import os
-import random
-import json
-from pymongo import MongoClient
-
-dotenv.load_dotenv
-db_str = "mongodb+srv://DartSams:Dartagnan_19@personal-cluser-db.qavgfkq.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(db_str)
-mydb = client["Personal-Cluster-db"] #connects to db but if not found will create it 
-table1 = mydb["hiders"] #connects to table but if not found will create it 
-# table2 = mydb["Quiz"]
+import time
 
 
-print(table1)
+load_dotenv()
 
-data = {
-    "username":"Maya",
-    "found":"Not yet",
-    "Image":"Image link"
-}
-table1.insert_one(data) #inserts into db
-client.close()
+cleardb_str = "mysql://bde8974ff6848a:f82355c4@us-cdbr-east-06.cleardb.net/heroku_03c2a5e7c8a7f77?reconnect=true"
+
+db=mysql.connector.connect(
+    host="us-cdbr-east-06.cleardb.net",
+    user="bde8974ff6848a",
+    passwd="f82355c4",
+    database="heroku_03c2a5e7c8a7f77"
+    )
+
+mycursor=db.cursor(buffered=True)
+
+# print(mycursor.execute("Show tables;"))
+
+def create_hider(hider_name):
+    mycursor.execute(f"CREATE TABLE {hider_name} (name VARCHAR(100),found VARCHAR(100),image_link VARCHAR(100))")
+
+
+# create_hider("Hiders")
+
+
+def insert_user(name,found,image_link):
+    # mycursor.execute("INSERT INTO Flask_Profile_Info (author,gender,age,job,location) VALUES (%s,%s%s,%s,%s)", ("iphone 69+","Dsams"))
+    # mycursor.execute("INSERT INTO Post_Table (author,post_date,post) VALUES (%s,%s,%s)", ("dartsams","Sat Jun 12 14:13:45 2021","i hate life"))
+    mycursor.execute("INSERT INTO Hiders (name,found,image_link) VALUES (%s,%s,%s)",(name,found,image_link))
+    db.commit()
+
+# insert_user("Maya","Not yet","Image")
+
+def show_entries(table_name):
+    user=[]
+    mycursor.execute(f"SELECT * FROM {table_name}")
+    for i in mycursor:
+        # print(i)
+        user.append(i)
+        # return i
+    return user
+
+x = show_entries('Hiders')
+print(x)
--- main.py
+++ main.py
@@ -26,10 +26,11 @@ app.config["SECRET_KEY"] = "hello"
 socketio = SocketIO(app)  # init the socket connection
 
 mysql = MySQL()  # to connect flask to mysql
-app.config["MYSQL_DATABASE_HOST"] = "us-cdbr-east-04.cleardb.com"
-app.config["MYSQL_DATABASE_USER"] = "bcc2ec4fcecbe5"
-app.config["MYSQL_DATABASE_PASSWORD"] = "Dartagnan19@"
-app.config["MYSQL_DATABASE_DB"] = "heroku_d10e4ce632a9633"
+cleardb_str = "mysql://bde8974ff6848a:f82355c4@us-cdbr-east-06.cleardb.net/heroku_03c2a5e7c8a7f77?reconnect=true"
+app.config["MYSQL_DATABASE_HOST"] = "us-cdbr-east-06.cleardb.net"
+app.config["MYSQL_DATABASE_USER"] = "bde8974ff6848a"
+app.config["MYSQL_DATABASE_PASSWORD"] = "f82355c4"
+app.config["MYSQL_DATABASE_DB"] = "heroku_03c2a5e7c8a7f77"
 mysql.init_app(app)  # init the flask  to mysql connection
 
 conn=mysql.connect()
@@ -38,14 +39,53 @@ mycursor=conn.cursor()
 def create_hider(hider_name):
     mycursor.execute(f"CREATE TABLE {hider_name} (name VARCHAR(100),found VARCHAR(100),image_link VARCHAR(100))")
 
+
+# create_hider("Hiders")
+
+
+def insert_user(name,found="Not Yet",image_link="default link"):
+    # mycursor.execute("INSERT INTO Flask_Profile_Info (author,gender,age,job,location) VALUES (%s,%s%s,%s,%s)", ("iphone 69+","Dsams"))
+    # mycursor.execute("INSERT INTO Post_Table (author,post_date,post) VALUES (%s,%s,%s)", ("dartsams","Sat Jun 12 14:13:45 2021","i hate life"))
+    mycursor.execute("INSERT INTO Hiders (name,found,image_link) VALUES (%s,%s,%s)",(name,found,image_link))
+    conn.commit()
+
+# insert_user("Maya","Not yet","Image")
+
+def show_entries(table_name):
+    user=[]
+    mycursor.execute(f"SELECT * FROM {table_name}")
+    for i in mycursor:
+        print(i)
+        user.append(i)
+        # return i
+    return user
+
+# show_entries('Hiders')
+
 @app.route("/")
 def index():
-    create_hider("Maya")
+
     data = {
         "participants_data":participants_data,
-        "participants_names":participants_names
+        "participants_names":participants_names,
+        # "participants":[]
     }
     return render_template("index.html",data=data)
+
+@socketio.on("new-participant")
+def handle_message(data):
+    print(data)
+    insert_user(data["name"])
+
+@socketio.on("refresh")
+def handle_message(msg):
+    hider_lst = show_entries('Hiders')
+    print(hider_lst)
+    data = {
+        "updated_list":hider_lst
+    }
+    emit("after-refresh",data,broadcast=True)
+
 
 
 if __name__ == "__main__":
